cc4ec478f5ca9b4e23ba4ea1fdd0ff506ecb9fb0
kickeyhickey <hickeydeveloper@gmail.com>
2024-03-17 20:27:01 -0700

    rid of unncecessary linter

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const container = document.getElementById('root');
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <StrictMode>
